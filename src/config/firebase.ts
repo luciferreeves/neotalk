73492f7a751abf65e3e9b51a733325d15f655fc5
Bobby <bobbyskhs@gmail.com>
2023-04-06 19:54:32 -0400

    Adding Firebase and Auth Callback to URL

---
+++ src/config/firebase.ts
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyB9WKqgini9TtmMnn4PCilt66xMA_c_7kA",
+    authDomain: "neocitiestalk.firebaseapp.com",
+    projectId: "neocitiestalk",
+    storageBucket: "neocitiestalk.appspot.com",
+    messagingSenderId: "205175432042",
+    appId: "1:205175432042:web:0ffe7e7fa96c0cd25ebb13",
+};

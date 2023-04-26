c5603b4853f6ea5d4ec5d3a84a14cc52210eac20
Bobby <bobbyskhs@gmail.com>
2023-04-25 23:31:09 -0400

    better dropdown handled using JS

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,24 @@ import { firebaseConfig } from './config/firebase';
 
 
 const app = initializeApp(firebaseConfig);
+const quickDropdown: HTMLElement | null = document.getElementById('quickDropdown');
 
-document.getElementById('quickLinks')?.addEventListener('click', () => {
-    document.getElementById('quickDropdown')?.classList.toggle('show');
+// Handle Quick Links Dropdown Behavior
+document.addEventListener('click', (e) => {
+    if (quickDropdown && quickDropdown.style.display === 'block') {
+        if (e.target !== quickDropdown && e.target !== document.getElementById('quickLinks')) {
+            quickDropdown.style.display = 'none';
+        }
+    }
 });
 
+document.getElementById('quickLinks')?.addEventListener('click', () => {
+    if (quickDropdown && quickDropdown.style.display === 'none') {
+        quickDropdown.style.display = 'block';
+    }
+    else if (quickDropdown) {
+        quickDropdown.style.display = 'none';
+    }
+});
 
 console.log(app);

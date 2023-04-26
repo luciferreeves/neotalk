c7668656edff74b7a5a9d19bf32a552fd7c77c6e
Bobby <bobbyskhs@gmail.com>
2023-04-25 23:24:42 -0400

    Basic Header

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,9 @@ import { firebaseConfig } from './config/firebase';
 
 const app = initializeApp(firebaseConfig);
 
+document.getElementById('quickLinks')?.addEventListener('click', () => {
+    document.getElementById('quickDropdown')?.classList.toggle('show');
+});
+
+
 console.log(app);
